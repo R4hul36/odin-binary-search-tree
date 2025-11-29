2d2e80ea7d665a3e8b4e4b2210d3357c6a08ef18
Rahul <rahul.wa47@gmail.com>
2025-11-29 13:05:47 +0530

    completed delete method

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -32,6 +32,7 @@ class Tree {
     if (root === null) {
       return new Node(value)
     }
+
     if (value < root.value) {
       root.left = this.insert(root.left, value)
     } else {
@@ -40,18 +41,49 @@ class Tree {
 
     return root
   }
+
+  getPredecessor(preNode) {
+    preNode = preNode.left
+    while (preNode !== null && preNode.right !== null) {
+      preNode = preNode.left
+    }
+    return preNode
+  }
+
+  delete(root, value) {
+    if (root === null) {
+      return root
+    }
+    if (root.value > value) {
+      root.left = this.delete(root.left, value)
+    } else if (root.value < value) {
+      root.right = this.delete(root.right, value)
+    } else {
+      if (root.left === null) {
+        return root.right
+      }
+      if (root.right === null) {
+        return root.left
+      }
+      const predecessor = this.getPredecessor(root)
+      root.value = predecessor.value
+      root.left = delete (root.left, predecessor.value)
+    }
+    return root
+  }
 }
 
-const tree = new Tree([1, 2, 3, 4, 5])
+const tree = new Tree([1, 2, 3, 4, 5, 6, 9])
 
-tree.insert(tree.root, 7)
+tree.insert(tree.root, 11)
 
+// tree.delete(tree.root, 2)
 const prettyPrint = (node, prefix = '', isLeft = true) => {
   if (node === null) {
     return
   }
+
   if (node.right !== null) {
-    //console.log(node.right, 'rightt')
     prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false)
   }
   console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`)
